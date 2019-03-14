1eb4a8706a839692c7be2e52f1303f23a69423e3
Kalle <karl.mortberg@gmail.com>
2019-03-14 15:55:57 +0100

    Multiball Script
    Pickup will spawn a second ball that will get destroyed when it goes out of bounds.

---
+++ Assets/Scripts/PickupScripts/MultiBallScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiBallScript : MonoBehaviour
+{
+    public float spinSpeed = 5;
+    public GameObject ball;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, spinSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponentInParent<Ball>() != null)
+        {
+            var ballInstance = Instantiate(ball);
+            ballInstance.GetComponent<Ball>().setMultiball(true);
+            Debug.Log("MULTIBALL!!");
+            Destroy(this.gameObject);
+        }
+    }
+}
