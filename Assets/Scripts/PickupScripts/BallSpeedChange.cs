29253421745f5bf034ce0453faf09297d1c2ead2
Kalle <karl.mortberg@gmail.com>
2019-03-15 17:00:17 +0100

    BallSpeedBoostPickup changes
    Changed name of the speedchanger pickup and moved it into  the pickups folder.
    
    Currently spawns randomly with a positive or negative speed change. Can be set by game manager

---
+++ Assets/Scripts/PickupScripts/BallSpeedChange.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpeedChange : MonoBehaviour
+{
+    public float SpeedMultiplier = 0.8f;
+    public float spinSpeed = 2;
+    public bool isSpeedIncreasing = true;
+
+    private SpriteRenderer sr;
+
+    private void Start()
+    {
+
+        sr = GetComponentInParent<SpriteRenderer>();
+
+        //Randomly determine if its increasing or decreasing mehehe
+        if (Random.value >= 0.5)
+        {
+            isSpeedIncreasing = false;
+            sr.color = Color.green;
+        }else
+        {
+            isSpeedIncreasing = true;
+            sr.color = Color.red;
+        }
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, spinSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponentInParent<Ball>() != null && isSpeedIncreasing)
+        {
+            collision.GetComponentInParent<Ball>().MultiplyVelocity(SpeedMultiplier + 0.4f);
+            Destroy(this.gameObject);
+        } else { 
+            collision.GetComponentInParent<Ball>().MultiplyVelocity(SpeedMultiplier);
+            Destroy(this.gameObject);
+        }
+    }
+
+    //So we can control this from game manager when spawning it
+    public void IsSpeedIncreasing (bool value)
+    {
+        isSpeedIncreasing = value;
+    }
+}
