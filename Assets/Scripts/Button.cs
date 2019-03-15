78c22245bc754d1f0e1a41c120b9e63b8b84ab45
Anton Skatt <k8asnakeload@gmail.com>
2019-03-15 13:44:52 +0100

    Main menu changes.

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour
+{
+    public GameObject[] images;
+
+    public void Start()
+    {
+        for(int i = 0; i < images.Length; i++)
+        {
+            images[i] = GetComponent<GameObject>();
+        }
+    }
+
+    public void OnMouseOver()
+    {
+        images[0].SetActive(false);
+        images[1].SetActive(true);
+    }
+
+    public void OnMouseExit()
+    {
+        images[0].SetActive(true);
+        images[1].SetActive(false);
+    }
+}
